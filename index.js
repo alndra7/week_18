3b284bae9612c3a56b10c01dc8834f9bdb0a2420
alndra7 <al.go9871@gmail.com>
2023-04-23 02:49:40 +0300

    in progress

---
+++ index.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let heroes = JSON.parse(json);
+    let heroesContent = "";
+    for (let hero of heroes) {
+        
+        heroesContent += `<div class="hero">
+        <h2> ${hero.name}</h2>
+        Ваша оценка:<br>
+        <div class="rating"> 
+        <input type="radio" name="${hero.name}" value="1">
+        <input type="radio" name="${hero.name}" value="2">
+        <input type="radio" name="${hero.name}" value="3">
+        <input type="radio" name="${hero.name}" value="4">
+        <input type="radio" name="${hero.name}" value="5">
+        </div>
+        <div>Вселенная: ${hero.universe}</div>
+        <div>Род занятий: ${hero.occupation}</div>
+        <div>Альтер-эго ${hero.alterego}</div>
+        <div>Друзья: ${hero.friends}</div>
+        <div>Суперсилы: ${hero.superpowers}</div>
+        <img src="${hero.url}" alt="hero image" class="img">
+        <div>Информация: ${hero.info}</div>
+        </div>`
+    }
+    document.getElementById("heroes-container").innerHTML = heroesContent;
+});
+
+
+document.addEventListener("DOMContentLoaded", function(event){
+
+    // document.querySelector('.rating').onclick = function(e) {
+    // let result = e.target.value;
+    // console.log (result);
+    // localStorage.setItem('rate', result);
+    // }
+    
+    let rating = document.querySelectorAll('.rating')
+    console.log(rating);
+    rating.forEach(() => {
+        document.querySelector('.rating').onclick = function(e) {
+            let result = e.target.value;
+            console.log (result);
+            localStorage.setItem('rate', result);
+            }  
+    })
+});
